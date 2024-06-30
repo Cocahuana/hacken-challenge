cbe3365d9c34480481774f0c50c1d2e705dad422
cocahuana <ezequieldominguez11@gmail.com>
2024-06-30 17:32:54 -0300

    Feat: added structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { DatePicker } from 'antd';
-
+import { useState } from "react";
+import {
+  Typography,
+  Flex,
+  Pagination,
+  PaginationProps,
+  Select,
+  Space,
+  Table,
+} from "antd";
+const { Text } = Typography;
 function App() {
-  const [count, setCount] = useState(0)
+  const onShowSizeChange: PaginationProps["onShowSizeChange"] = (
+    current,
+    pageSize
+  ) => {
+    console.log(current, pageSize);
+  };
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+  const onSearch = (value: string) => {
+    console.log("search:", value);
+  };
+  const onChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Current Price",
+      dataIndex: "Current Price",
+      key: "Current Price",
+    },
+    {
+      title: "Circulating Supply",
+      dataIndex: "Circulating Supply",
+      key: "Circulating Supply",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <DatePicker />
+      <Flex
+        style={{ gap: "24px", minHeight: "100vh" }}
+        align="stretch"
+        vertical
+      >
+        <Text style={{ fontSize: "24px", marginTop: "24px" }}>
+          Coins & Markets
+        </Text>
+        <Space wrap>
+          <Select
+            defaultValue="USD"
+            style={{ width: 120 }}
+            // onChange={handleChange}
+            options={[
+              { value: "USD", label: "USD" },
+              { value: "EUR", label: "EUR" },
+            ]}
+          />
+        </Space>
+        <Space wrap>
+          <Select
+            defaultValue="descending"
+            style={{ width: 120 }}
+            // onChange={handleChange}
+            options={[
+              { value: "ascending", label: "Market cap ascending" },
+              { value: "descending", label: "Market cap descending" },
+            ]}
+          />
+        </Space>
+        <Table dataSource={dataSource} columns={columns} />;
+        {/* <Pagination
+          showSizeChanger
+          onShowSizeChange={onShowSizeChange}
+          defaultCurrent={3}
+          total={500}
+        /> */}
+        <Select
+          showSearch
+          placeholder="10 / page"
+          optionFilterProp="label"
+          onChange={onChange}
+          onSearch={onSearch}
+          options={[
+            {
+              value: "5 / page",
+              label: "5 / page",
+            },
+            {
+              value: "10 / page",
+              label: "10 / page",
+            },
+            {
+              value: "20 / page",
+              label: "20 / page",
+            },
+            {
+              value: "50 / page",
+              label: "50 / page",
+            },
+            {
+              value: "100 / page",
+              label: "100 / page",
+            },
+          ]}
+        />
+      </Flex>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
